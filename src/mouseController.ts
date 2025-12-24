61edfc1d852b214b2b257242f3f2f04683c47cbf
TangFg <1432837709@qq.com>
2025-12-24 18:17:59 +0800

    Fix background click class name conflict with unique ID

--- src/mouseController.ts
+++ src/mouseController.ts
@@ -6,112 +6,45 @@ const execAsync = promisify(exec);
 /**
  * PowerShell script for background click (sends message to window without moving mouse)
  */
-const BACKGROUND_CLICK_SCRIPT = (hwnd: number, x: number, y: number, button: 'left' | 'right' = 'left') => `
-Add-Type @"
+const BACKGROUND_CLICK_SCRIPT = (hwnd: number, x: number, y: number, button: 'left' | 'right' = 'left') => {
+  const uid = Date.now();
+  return `
+Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
 
-public class BackgroundClicker {
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
+public class BC${uid} {
     [DllImport("user32.dll")]
     public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-    [DllImport("user32.dll")]
-    public static extern IntPtr ChildWindowFromPointEx(IntPtr hWndParent, POINT pt, uint uFlags);
-
-    [DllImport("user32.dll")]
-    public static extern bool ScreenToClient(IntPtr hWnd, ref POINT lpPoint);
-
-    [DllImport("user32.dll")]
-    public static extern bool ClientToScreen(IntPtr hWnd, ref POINT lpPoint);
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct POINT {
-        public int X;
-        public int Y;
-        public POINT(int x, int y) { X = x; Y = y; }
-    }
-
-    public const uint WM_LBUTTONDOWN = 0x0201;
-    public const uint WM_LBUTTONUP = 0x0202;
-    public const uint WM_RBUTTONDOWN = 0x0204;
-    public const uint WM_RBUTTONUP = 0x0205;
-    public const uint WM_MOUSEMOVE = 0x0200;
-    public const uint MK_LBUTTON = 0x0001;
-    public const uint MK_RBUTTON = 0x0002;
-    public const uint CWP_ALL = 0x0000;
-    public const uint CWP_SKIPDISABLED = 0x0002;
-    public const uint CWP_SKIPINVISIBLE = 0x0001;
 
     public static IntPtr MakeLParam(int x, int y) {
         return (IntPtr)((y << 16) | (x & 0xFFFF));
     }
 
-    public static void BackgroundClick(IntPtr parentHwnd, int clientX, int clientY, bool rightClick = false) {
-        // Try to find the deepest child window at this point
-        POINT pt = new POINT(clientX, clientY);
-        IntPtr targetHwnd = ChildWindowFromPointEx(parentHwnd, pt, CWP_SKIPINVISIBLE);
-
-        int targetX = clientX;
-        int targetY = clientY;
-
-        // If we found a child window, convert coordinates
-        if (targetHwnd != IntPtr.Zero && targetHwnd != parentHwnd) {
-            // Convert from parent client to screen
-            POINT screenPt = new POINT(clientX, clientY);
-            ClientToScreen(parentHwnd, ref screenPt);
-            // Convert from screen to child client
-            ScreenToClient(targetHwnd, ref screenPt);
-            targetX = screenPt.X;
-            targetY = screenPt.Y;
-        } else {
-            targetHwnd = parentHwnd;
-        }
-
-        IntPtr lParam = MakeLParam(targetX, targetY);
-
-        // Send mouse move first
-        PostMessage(targetHwnd, WM_MOUSEMOVE, IntPtr.Zero, lParam);
-        System.Threading.Thread.Sleep(10);
-
-        if (rightClick) {
-            PostMessage(targetHwnd, WM_RBUTTONDOWN, (IntPtr)MK_RBUTTON, lParam);
-            System.Threading.Thread.Sleep(30);
-            PostMessage(targetHwnd, WM_RBUTTONUP, IntPtr.Zero, lParam);
-        } else {
-            PostMessage(targetHwnd, WM_LBUTTONDOWN, (IntPtr)MK_LBUTTON, lParam);
-            System.Threading.Thread.Sleep(30);
-            PostMessage(targetHwnd, WM_LBUTTONUP, IntPtr.Zero, lParam);
-        }
-    }
-
-    // Alternative: click directly to parent window without child lookup
-    public static void DirectClick(IntPtr hwnd, int clientX, int clientY, bool rightClick = false) {
+    public static void Click(IntPtr hwnd, int clientX, int clientY, bool rightClick) {
         IntPtr lParam = MakeLParam(clientX, clientY);
 
-        PostMessage(hwnd, WM_MOUSEMOVE, IntPtr.Zero, lParam);
+        // Mouse move
+        PostMessage(hwnd, 0x0200, IntPtr.Zero, lParam);
         System.Threading.Thread.Sleep(10);
 
         if (rightClick) {
-            PostMessage(hwnd, WM_RBUTTONDOWN, (IntPtr)MK_RBUTTON, lParam);
+            PostMessage(hwnd, 0x0204, (IntPtr)0x0002, lParam);
             System.Threading.Thread.Sleep(30);
-            PostMessage(hwnd, WM_RBUTTONUP, IntPtr.Zero, lParam);
+            PostMessage(hwnd, 0x0205, IntPtr.Zero, lParam);
         } else {
-            PostMessage(hwnd, WM_LBUTTONDOWN, (IntPtr)MK_LBUTTON, lParam);
+            PostMessage(hwnd, 0x0201, (IntPtr)0x0001, lParam);
             System.Threading.Thread.Sleep(30);
-            PostMessage(hwnd, WM_LBUTTONUP, IntPtr.Zero, lParam);
+            PostMessage(hwnd, 0x0202, IntPtr.Zero, lParam);
         }
     }
 }
 "@
 
-$hwnd = [IntPtr]${hwnd}
-# Try direct click first (simpler, works for many apps)
-[BackgroundClicker]::DirectClick($hwnd, ${x}, ${y}, ${button === 'right' ? '$true' : '$false'})
+[BC${uid}]::Click([IntPtr]${hwnd}, ${x}, ${y}, ${button === 'right' ? '$true' : '$false'})
 @{ success = $true } | ConvertTo-Json -Compress
 `;
+};
 
 /**
  * PowerShell script template for mouse click (moves mouse)
